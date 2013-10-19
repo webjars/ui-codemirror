5da5ebf06d71b3f87041a0d78ad0f2c072c2cfa4
James Ward <james@jamesward.org>
2013-10-19 10:38:10 -0600

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+WebJar for ui-codemirror
+
+More info: http://webjars.org
+
+Upstream: https://github.com/angular-ui/ui-codemirror
---
+++ src/main/resources/webjars-requirejs.js
@@ -0,0 +1,7 @@
+/*global requirejs */
+
+requirejs.config({
+  shim: {
+    'ui-codemirror': [ 'webjars!codemirror.js', 'webjars!angular.js' ]
+  }
+});
